833ab5746c2f5df48d4b4048e88e79bbfcf29a55
Ryan Guide <ryan.guide@comcast.net>
2021-06-08 16:13:36 -0400

    Added Create and Read methods

--- album_ratings/models.py
+++ album_ratings/models.py
@@ -4,5 +4,8 @@ class AlbumModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     artist = db.Column(db.String(50))
-    release_year = db.Column(db.Integer(4))
+    release_year = db.Column(db.Integer)
     rating = db.Column(db.String(1))
+
+    def __repr__(self):
+        return f"AlbumModel('{self.name}', '{self.artist}')"
