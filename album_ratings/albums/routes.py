3b6b321f32c53487e499ee3f0c2f23d19d82329a
Ryan Guide <ryan.guide@comcast.net>
2021-06-08 16:55:26 -0400

    Finished CRUD methods and added basic style

--- album_ratings/albums/routes.py
+++ album_ratings/albums/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, Blueprint, flash, redirect, url_for
+from flask import render_template, Blueprint, flash, redirect, url_for, request
 from album_ratings.albums.forms import AlbumForm
 from album_ratings.models import AlbumModel
 from album_ratings import db
@@ -26,10 +26,30 @@ def create():
         return redirect(url_for('albums.home'))
     return render_template('album.html', title='Add Album', form=form, legend="Add a new Album")
 
-@albums.route('/update')
-def update():
-    pass
+@albums.route('/update/<int:album_id>', methods = ['GET', 'POST'])
+def update(album_id):
+    album = AlbumModel.query.get_or_404(album_id)
+    form = AlbumForm()
+    if form.validate_on_submit():
+        album.name = form.album_name.data
+        album.artist = form.artist.data
+        album.release_year = form.release_year.data
+        album.rating = form.rating.data
+        db.session.commit()
+        flash('Changes have been saved!', 'success')
+        return redirect(url_for('albums.home'))
+    elif request.method == 'GET':
+        form.album_name.data = album.name
+        form.artist.data = album.artist
+        form.release_year.data = album.release_year
+        form.rating.data = album.rating
+    return render_template('album.html', title="Edit Album Info", form=form, legend='Update Album Info')
 
-@albums.route('/delete')
-def delete():
-    pass
+
+@albums.route('/delete/<album_id>', methods=['GET','POST'])
+def delete(album_id):
+    album = AlbumModel.query.get_or_404(album_id)
+    db.session.delete(album)
+    db.session.commit()
+    flash('Album deleted.', 'success')
+    return redirect(url_for('albums.home'))
